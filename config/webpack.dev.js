22e44079fb7cce5e557036505a9c2886f2698964
Hugo Cardoso <hugorafaelmc@gmail.com>
2019-07-26 19:25:02 -0300

    Added network ip from host in dev server config

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ip = require("ip");
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, '../dist')
+    contentBase: path.resolve(__dirname, '../dist'),
+    host: ip.address(),
+    port: 8080
   },
 });
