aa89669e62177f0ee18df8423eb20bc7aa437280
Hugo Cardoso <hugorafaelmc@gmail.com>
2019-07-25 21:49:17 -0300

    Added external breakpoints

--- src/globalStyles/Container.js
+++ src/globalStyles/Container.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import breakPoints from '@globalStyles/breakPoints';
 
 export default styled.div`
   width: 100%;
   max-width: 1230px;
   margin: 0 auto;
   
-  @media screen and (max-width: 980px) {
+  @media screen and (max-width: ${ breakPoints.tablet }px) {
     padding: 0 5px;
   }
 `;
