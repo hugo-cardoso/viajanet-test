b4aabbe79a2acc4955f25f844564415a7ce1ee6b
Hugo Cardoso <hugorafaelmc@gmail.com>
2019-07-27 06:25:29 -0300

    Fixed webpack devserver ip

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ip = require("ip");
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, '../build'),
-    host: ip.address(),
+    host: '0.0.0.0',
     port: 8080
   },
 });
