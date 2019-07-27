ef4846f95f85128e5b8498b6d8ca553b0f774642
Hugo Cardoso <hugorafaelmc@gmail.com>
2019-07-26 22:39:33 -0300

    Added response mock for airline promotions

---
+++ src/promotionsMock.js
@@ -0,0 +1,90 @@
+import latamIcon from '@images/companies/latam.gif';
+import golIcon from '@images/companies/gol.gif';
+
+export default [
+  {
+    title: "São Paulo para Foz do Iguaçu",
+    thumbnailUrl: "https://www.viajanet.com.br/static/assets/platform/img/catalog/IGU/foz-iguacu.jpg",
+    price: 297,
+    type: "Passagens aéreas",
+    dates: {
+      going: {
+        day: 23,
+        month: "ago"
+      },
+      return: {
+        day: 26,
+        month: "ago"
+      }
+    },
+    company: {
+      name: "Latam",
+      logo: latamIcon
+    },
+    discountPercent: 59,
+    url: "https://www.viajanet.com.br/busca/passagens/voos#/SAO/IGU/RT/27-08-2019/30-08-2019/-/-/-/1/0/0/-/-/BRL/-"
+  },{
+    title: "São Paulo para Rio de Janeiro",
+    thumbnailUrl: "https://www.viajanet.com.br/static/assets/platform/img/catalog/RIO/rio-de-janeiro-nova.jpg",
+    price: 254,
+    type: "Passagens aéreas",
+    dates: {
+      going: {
+        day: 5,
+        month: "set"
+      },
+      return: {
+        day: 5,
+        month: "out"
+      }
+    },
+    company: {
+      name: "Gol",
+      logo: golIcon
+    },
+    discountPercent: 39,
+    url: "https://www.viajanet.com.br/busca/passagens/voos#/SAO/RIO/RT/05-09-2019/08-09-2019/-/-/-/1/0/0/-/-/BRL/-"
+  },{
+    title: "São Paulo para Porto Seguro",
+    thumbnailUrl: "https://www.viajanet.com.br/static/assets/platform/img/catalog/BPS/porto-seguro.jpg",
+    price: 299,
+    type: "Passagens aéreas",
+    dates: {
+      going: {
+        day: 4,
+        month: "ago"
+      },
+      return: {
+        day: 7,
+        month: "ago"
+      }
+    },
+    company: {
+      name: "Gol",
+      logo: golIcon
+    },
+    discountPercent: 42,
+    url: "https://www.viajanet.com.br/busca/passagens/voos#/SAO/BPS/RT/04-08-2019/07-08-2019/-/-/-/1/0/0/-/-/BRL/-"
+  },{
+    title: "São Paulo para Brasília",
+    thumbnailUrl: "https://www.viajanet.com.br/static/assets/platform/img/catalog/BSB/brasilia.jpg",
+    price: 248,
+    type: "Passagens aéreas",
+    dates: {
+      going: {
+        day: 31,
+        month: "out"
+      },
+      return: {
+        day: 3,
+        month: "nov"
+      }
+    },
+    company: {
+      name: "Latam",
+      logo: latamIcon
+    },
+    discountPercent: 33,
+    url: "https://www.viajanet.com.br/busca/passagens/voos#/SAO/BSB/RT/31-10-2019/03-11-2019/-/-/-/1/0/0/-/-/BRL/-"
+  }
+];
